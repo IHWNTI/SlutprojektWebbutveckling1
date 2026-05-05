ed1f2091609c1268aa6cd4742147aac8b632761f
IHWNTI <isak.hjelmstedtwikstrom@elev.ga.ntig.se>
2026-05-05 13:25:15 +0200

    Minor CSS work

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -1,5 +1,17 @@
 
 
 export default function SettingsPanel() {
+    return (
+
+        <div className="settingsPanel">
+            <ul>
+                <li>Item1</li>
+                <li>Item2</li>
+            </ul>
+
+        </div>
+
     
+    
+    );
 }
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,5 +1,6 @@
 import BusMarkers from "../components/BusMarkers";
 import MapView from "../components/MapView";
+import SettingsPanel from "../components/SettingsPanel";
 import TopSearch from "../components/TopSearchBar";
 import "./MapPage.css";
 
@@ -7,15 +8,16 @@ export default function MapPage() {
   return (
     <div className="container">
       <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/map">Map</a></li>
-                </ul>
-            </nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/map">Map</a></li>
+        </ul>
+      </nav>
       <TopSearch />
       <MapView>
         <BusMarkers />
       </MapView>
+      <SettingsPanel/>
     </div>
 
   );
